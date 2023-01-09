f09e2f47106a09b4ec6a5925c2e1c63374b81134
maxxAbow <mbowman@welland.com>
2023-01-09 18:34:39 -0500

    update

--- Develop/script.js
+++ Develop/script.js
@@ -3,3 +3,18 @@ var today = document.getElementById('currentDay');
 
 today.innerHTML=moment().format('LLLL');
 
+//Background colors
+var rightNow = moment().format('LT');
+var hourHeader = document.getElementsByClassName('hour')
+let theTime = hourHeader.getAttribute('data-value');
+
+// if (theTime == rightNow){
+//     document.getElementsByClassName('hour').style.present
+// }
+// else if (theTime > rightNow){
+//     document.getElementsByClassName('hour').style.future
+// }
+// else if (theTime < rightNow){
+//     document.getElementsByClassName('hour').style.past
+// };
+
